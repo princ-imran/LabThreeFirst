d9d2a43eb5de327a36a1a2f4660399910e8e990f
Imran Hoshain <princ.imran@gmail.com>
2020-03-02 09:35:10 +0600

    Array in Java

---
+++ src/labthreefirst/LabThreeFirst.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labthreefirst;
+
+/**
+ *
+ * @author Spy Hacker
+ */
+public class LabThreeFirst {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int month_days[];
+        month_days = new int[10];
+        month_days[0] = 31;
+        month_days[1] = 28;
+        month_days[2] = 31;
+        month_days[3] = 30;
+        System.out.println("February is: "+month_days[1]);        
+    
+    }
+}
